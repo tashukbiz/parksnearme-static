fe1f8870f475e26fda3cddbfa54accadb433e59e
tashuk <tashukwork@gmail.com>
2026-07-13 22:38:39 +1000

    installed monetag

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11279575
-}
-self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+  domain: '3nbf4.com',
+  zoneId: 11279676,
+};
+self.lary = '';
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw');
